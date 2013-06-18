6c8279f523a112e22f7612302ba072ef97dba641
exiton3 <exiton3@gmail.com>
2013-06-18 13:32:44 +0000

    refactoring: removed ClassMapping from PropertyMapInfo

--- Mapper/IPropertyMapOptions.cs
+++ Mapper/IPropertyMapOptions.cs
@@ -3,6 +3,5 @@ namespace Mapper
     public interface IPropertyMapOptions : IFluentSyntax
     {
         void UseFormatter<TFormatter>() where TFormatter : IValueFormatter, new();
-        void UseMapping<TClassMap>() where TClassMap : IMapConfiguration;
     }
 }
