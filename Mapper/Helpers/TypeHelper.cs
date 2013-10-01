7456343fea6f19ea091c228f73ab1e1f44c1dc88
exiton3 <exiton3@gmail.com>
2013-10-01 09:04:20 +0000

    refactoring

---
+++ Mapper/Helpers/TypeHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mapper.Helpers
+{
+    public static class TypeHelper
+    {
+        public static bool IsNullableType(this Type type)
+        {
+            return type.IsGenericType && (type.GetGenericTypeDefinition().Equals(typeof(Nullable<>)));
+        }
+    }
+}
