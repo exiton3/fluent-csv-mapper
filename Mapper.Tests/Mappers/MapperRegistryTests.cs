faad84db7c94108628f745a58584beb7dc7a7bde
exiton3 <exiton3@gmail.com>
2013-10-07 13:14:21 +0000

    Fixed creation of Mappers one time

---
+++ Mapper.Tests/Mappers/MapperRegistryTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Mapper.Mappers;
+using NUnit.Framework;
+
+namespace Mapper.Tests.Mappers
+{
+    [TestFixture]
+    public class MapperRegistryTests
+    {
+        [Test]
+        public void GetAllMappingsRetursOneInstanceOfMapperAnyTime()
+        {
+            var mapperRegistry = new MapperRegistry();
+
+            IMapper first = mapperRegistry.GetAllMappers().First();
+            IMapper second = mapperRegistry.GetAllMappers().First();
+
+            Assert.That(first, Is.EqualTo(second));
+        }
+    }
+}
--- Mapper/Mappers/MapperRegistry.cs
+++ Mapper/Mappers/MapperRegistry.cs
@@ -4,9 +4,16 @@ namespace Mapper.Mappers
 {
    public class MapperRegistry : IMapperRegistry
     {
+       private readonly List<IMapper> _mappers;
+
+       public MapperRegistry()
+       {
+           _mappers = new List<IMapper> { new ValueMapper(), new ReferenceMapper(), new CollectionMapper(), new NullableMapper() };
+           
+       }
         public IEnumerable<IMapper> GetAllMappers()
         {
-            return new List<IMapper> {new ValueMapper(), new ReferenceMapper(), new CollectionMapper(), new NullableMapper()};
+            return _mappers;
         }
     }
 }
